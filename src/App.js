1a7cb859915b5ae1f03efdf30fac154c0f572254
pranjal singh <suryakantsharma.me@gmail.com>
2021-08-20 20:39:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Home from './components/home';
+import QuizSection from './components/quizSection';
+import AboutMe from './components/ContactUs';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/aboutme" component={AboutMe} />
+          <Route exact path="/:type" component={QuizSection} />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
